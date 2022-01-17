4168d8ee7ea760e8931236d2e4022d403d561265
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-17 16:16:24 +0300

    Initializing work with firebase admin

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var defaultApp = FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.FromFile("notify-69147-firebase-adminsdk-uwamm-643fdf9d38.json"),
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Данный проект находится в разработке.
 
 Swagger: http://localhost/swagger/index.html
+
+### For developers
+
+For developers
+Before working with the server, create a **private key** in the Firebase cabinet and specify the path to it in `Program.cs`
