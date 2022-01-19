c2125b3c0d39682d73dbfccf1b14f0c2c662ba82
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-19 10:52:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using NotifyServer.Migrations;
+using NotifyServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +16,19 @@ var defaultApp = FirebaseApp.Create(new AppOptions()
     Credential = GoogleCredential.FromFile("notify-69147-firebase-adminsdk-uwamm-643fdf9d38.json"),
 });
 
+builder.Services.AddDbContext<AppDbContext>(optionsAction: options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), optionsBuilder => optionsBuilder.MigrationsAssembly()));
+
+Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, InitialCreate>());
+builder.Services.AddScoped<INotifyUserRepository, NotifyUserRepository>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
