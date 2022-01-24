19f9835b3d963eedb09dd7d2b51825d628f72313
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-24 12:27:49 +0300

    Update AppDBContext.cs

--- Models/AppDBContext.cs
+++ Models/AppDBContext.cs
@@ -4,17 +4,11 @@ namespace NotifyServer.Models
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options){
 
-    }
-
-        public DbSet<NotifyUser> Users { get; set; } = null!;
-
-        protected new void OnModelCreating(ModelBuilder modelBuilder)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<NotifyUser>().HasKey("Id");
+
         }
+        public DbSet<NotifyUser> Users { get; set; } = null!;
     }
 }
