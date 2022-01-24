620648c68e3ffa2a958e370cf5e98804ed28401b
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-24 12:28:17 +0300

    Edit Migrations

---
+++ Migrations/20220123211443_InitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotifyServer.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Firstname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Lastname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Color = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotifyUserNotifyUser",
+                columns: table => new
+                {
+                    SubscribersId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubscriptionsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotifyUserNotifyUser", x => new { x.SubscribersId, x.SubscriptionsId });
+                    table.ForeignKey(
+                        name: "FK_NotifyUserNotifyUser_Users_SubscribersId",
+                        column: x => x.SubscribersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotifyUserNotifyUser_Users_SubscriptionsId",
+                        column: x => x.SubscriptionsId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotifyUserNotifyUser_SubscriptionsId",
+                table: "NotifyUserNotifyUser",
+                column: "SubscriptionsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotifyUserNotifyUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
