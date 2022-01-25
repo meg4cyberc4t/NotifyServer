aabb701ad5839b24f11d9ea6464ded64b436789a
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-25 18:22:16 +0300

    Update AppDBContext, Migrations

--- Models/AppDBContext.cs
+++ Models/AppDBContext.cs
@@ -9,5 +9,21 @@ namespace NotifyServer.Models
         }
 
         public DbSet<NotifyUser> Users { get; set; } = null!;
+        public DbSet<NotifyNotification> Notifications { get; set; } = null!;
+        public DbSet<NotifyFolder> Folders { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<NotifyFolder>()
+                .HasOne(e => e.Creator)
+                .WithMany(e => e.FolderWhereCreator);
+
+            modelBuilder.Entity<NotifyNotification>()
+                .HasOne(e => e.Creator)
+                .WithMany(e => e.NotificationsWhereCreator);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
