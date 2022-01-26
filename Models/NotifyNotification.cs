60d01f258a7679f398c36a0f922c53befee0b0fc
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-26 15:29:25 +0300

    Add NotifyNotificationInput and NotifyNotificationDetailed

--- Models/NotifyNotification.cs
+++ Models/NotifyNotification.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NotifyServer.Models;
 
@@ -12,13 +11,49 @@ public enum RepeatMode
     Everyyear,
 }
 
+public record NotifyNotificationInput(
+    [StringLength(50)] [Required] string Title,
+    string Description,
+    [Required] RepeatMode RepeatMode,
+    [Required] DateTime Deadline,
+    [Required] bool Important
+);
+
+public record NotifyNotificationDetailed(
+    Guid Id,
+    [StringLength(50)] [Required] string Title,
+    string Description,
+    bool Important,
+    RepeatMode RepeatMode,
+    DateTime Deadline,
+    NotifyUserQuick Creator,
+    int UniqueClaim
+);
+
 public class NotifyNotification
 {
     public Guid Id { get; set; }
     [StringLength(50)] [Required] public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
     public ICollection<NotifyUser> Participants { get; set; } = null!;
+    public bool Important;
     public RepeatMode RepeatMode { get; set; } = RepeatMode.None;
     public DateTime Deadline { get; set; }
     public NotifyUser Creator { get; set; } = null!;
+
+    public int UniqueClaim { get; init; }
+
+    public NotifyNotificationDetailed ToNotifyNotificationDetailed()
+    {
+        return new NotifyNotificationDetailed(
+            Id: Id,
+            Title: Title,
+            Description: Description,
+            Important: Important,
+            RepeatMode: RepeatMode,
+            Deadline: Deadline,
+            Creator: Creator.ToNotifyUserQuick(),
+            UniqueClaim: UniqueClaim
+        );
+    }
 }
