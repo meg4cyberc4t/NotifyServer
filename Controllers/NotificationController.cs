6a8daa0c38cd80ca30f3addb9fd65c7fb349f14f
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-26 15:36:05 +0300

    Add NotificationController
    Create, Get, GetById methods

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotifyServer.Models;
+using NotifyServer.Repository;
+
+namespace NotifyServer.Controllers;
+
+[Authorize]
+[Route("notifications")]
+[ApiController]
+public class NotificationController : Controller
+{
+    private readonly NotifyNotificationRepository _notificationRepository;
+
+    public NotificationController(AppDbContext context)
+    {
+        _notificationRepository = new NotifyNotificationRepository(context);
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<NotifyNotificationDetailed>> GetMyNotifications()
+    {
+        var user = (HttpContext.Items["User"] as NotifyUser)!;
+        return Ok(_notificationRepository.GetUserNotifications(user).Select(e => e.ToNotifyNotificationDetailed()));
+    }
+
+    [HttpGet("{id:guid}", Name = "GetNotificationById")]
+    public ActionResult<NotifyNotificationDetailed> Get(Guid id)
+    {
+        var user = (HttpContext.Items["User"] as NotifyUser)!;
+        var ntf = _notificationRepository.Get(id);
+        if (ntf == null)
+        {
+            return NotFound();
+        }
+
+        if (!ntf.Participants.Contains(user))
+        {
+            return Forbid();
+
+        }
+        return Ok(ntf.ToNotifyNotificationDetailed());
+
+    }
+
+    [HttpPost]
+    public ActionResult<NotifyNotificationDetailed> Create([FromBody] NotifyNotificationInput input)
+    {
+        var user = (HttpContext.Items["User"] as NotifyUser)!;
+        return Ok(_notificationRepository.Create(user, input).Entity.ToNotifyNotificationDetailed());
+    }
+}
