81db211a56277fbef8a4ab35efb50cca85e5ad49
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-04 00:01:50 +0300

    Revert "Fix "Follow" field in NotifyUserDetailed"
    This reverts commit f0ca20358b870d1217b3647f0d5fbaa6af12276c.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -21,14 +21,13 @@ public class UsersController : ControllerBase
     [HttpGet("{id:guid}", Name = "GetUserById")]
     public async Task<ActionResult<NotifyUserDetailed>> Get(Guid id)
     {
-        var requestUser = (HttpContext.Items["User"] as NotifyUser)!;
         var user = await _users.GetUserAsync(id);
         if (user == null)
         {
             return NotFound();
         }
 
-        return Ok(user.ToNotifyUserDetailed(requestUser));
+        return Ok(user.ToNotifyUserDetailed(user));
     }
 
     [HttpGet("{id:guid}/subscriptions")]
