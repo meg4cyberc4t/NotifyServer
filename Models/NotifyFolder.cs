60134ccdb92e3419923d8b88c9455dc402e3bd3a
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-29 10:10:26 +0300

    Add NotifyFolderQuick and modify NotifyUserDetailed

--- Models/NotifyFolder.cs
+++ Models/NotifyFolder.cs
@@ -7,12 +7,21 @@ public record NotifyFolderInput(
     string Description
 );
 
-public record NotifyFolderDetailed(
+public record NotifyFolderQuick(
     Guid Id,
     string Title,
     string Description,
     int ParticipantsCount,
     int NotificationsCount
+);
+
+public record NotifyFolderDetailed(
+    Guid Id,
+    string Title,
+    string Description,
+    int ParticipantsCount,
+    IEnumerable<NotifyNotificationQuick> Notifications,
+    NotifyUser Creator
 );
 
 public class NotifyFolder
@@ -24,9 +33,25 @@ public class NotifyFolder
     public ICollection<NotifyNotification> NotificationsList { get; init; } = null!;
     public NotifyUser Creator { get; init; } = null!;
 
+    public NotifyFolderQuick ToNotifyFolderQuick()
+    {
+        return new NotifyFolderQuick(
+            Title: Title, 
+            Id:Id, 
+            Description:Description, 
+            ParticipantsCount: Participants.Count, 
+            NotificationsCount: NotificationsList.Count);
+    }
+    
+
     public NotifyFolderDetailed ToNotifyFolderDetailed()
     {
-        return new NotifyFolderDetailed(Id: Id, Title: Title, Description: Description,
-            ParticipantsCount: Participants.Count, NotificationsCount: NotificationsList.Count);
+        return new NotifyFolderDetailed(
+            Id: Id, 
+            Title: Title, 
+            Description: Description,
+            ParticipantsCount: Participants.Count, 
+            Notifications: NotificationsList.Select(e => e.ToNotifyNotificationQuick()), 
+            Creator: Creator);
     }
 }
